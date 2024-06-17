2769698c95421fe5b5df354a9dbf197e4a0f41cf
Pulkit Kumar <pulkit@laptop99.tresata.com>
2024-06-17 12:36:23 +0530

    added scrape file to get the required fields from the post

---
+++ scrape.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Define the URL and query parameters
+URL="https://community.tresata.com/posts.json"
+
+# Define the API key and username
+API_KEY="bb962f04bfc55701405c0ad38587f60e5784999be7d5fcced57d37d91a23651c"
+API_USERNAME="system"
+FIRST_POST_ID=2
+OUTPUT_FILE="./documents/extracted-posts.txt"
+
+# Execute the curl command and store the response
+response=$(curl -s -X GET "$URL" -H "Api-Key: $API_KEY" -H "Api-Username: $API_USERNAME")
+
+# Define the output file and add header
+echo "id,username,user_title,raw, moderator, admin, staff, trust_level, topic_title, category_id" > $OUTPUT_FILE
+
+# Use jq to extract the last id from the latest_posts array
+LAST_POST_ID=$(echo "$response" | jq '.latest_posts | last | .id')
+
+CURR_POST_ID=$LAST_POST_ID
+
+# Fetch posts in a loop until we reach the first post ID
+while [ "$CURR_POST_ID" -gt "$FIRST_POST_ID" ]; do
+  # Fetch the current batch of posts
+  response=$(curl -s -X GET "$URL?before=$CURR_POST_ID" -H "Api-Key: $API_KEY" -H "Api-Username: $API_USERNAME")
+
+  # Extract posts information using jq
+  posts=$(echo "$response" | jq -r '.latest_posts[] | [.id, .username, .user_title, .raw, .moderator,.admin, .staff, .trust_level, .topic_title, .category_id] | @tsv' | sed 's/\t/|/g')
+
+  # Append extracted information to the output file with a line break after each entry
+  while IFS= read -r post; do
+    echo "$posts" >> $OUTPUT_FILE
+  done <<< "$posts"
+
+  # Update CURR_POST_ID to the ID of the last post in the current batch
+  CURR_POST_ID=$(echo "$response" | jq '.latest_posts | last | .id')
+done
+
+
