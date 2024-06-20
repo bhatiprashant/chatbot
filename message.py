e10e9d14b500121cd04d907aa4681d322ee40e19
Prashant Bhati <Prashant.bhati@tresata.com>
2024-06-20 11:54:04 +0530

    system message update

---
+++ message.py
@@ -0,0 +1,14 @@
+SYSTEM_MESSAGE = """ You are a Copilot for Tresata Data integration software. You are provided with the information about the software's 7 steps:
+1. Source
+2. Profile
+3. Prepare
+4. Connect
+5. Validate
+6. Enrich
+7. Orchestrate
+
+Follow these instructions when answering a query:
+1. If you have the information stored in embeddings, respond as a copilot for a software application.
+2. If the information is not available, reply: "Sorry, the Tresata community doesn’t contain the required information to answer your question. Please let me know if you have any other questions."
+
+"""
--- query.py
+++ query.py
@@ -1,10 +1,11 @@
 from model import LLMModel
 from llama_index.core import SimpleDirectoryReader
 from llama_index.core import Document
+from message import SYSTEM_MESSAGE
 
 # Load Document
 documents = SimpleDirectoryReader(
-    input_files = ["./documents/brd.pdf"]
+    input_files = ["./documents/tresataSupportPosts.txt"]
 ).load_data()
 documents = Document(text = "\n\n".join([doc.text for doc in documents]))
 
@@ -22,8 +23,9 @@ query_engine = chatbot.get_query_engine(vector_index)
 
 # Query the model
 while True:
-    query=input("ask your query:")
-    response = query_engine.query(query)
+    query=input("Ask your Query: ")
+    merged_query = f"{SYSTEM_MESSAGE}\nQuery: {query}"
+    response = query_engine.query(merged_query)
     print(response)
     print("\n")
 
